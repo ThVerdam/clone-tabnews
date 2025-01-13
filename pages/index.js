202ef409f2502c29ec48550e5af9d47fed3c1d53
Thomas Verdam <thomasverdam@hotmail.com>
2025-01-13 11:22:17 -0300

    adiciona scripts `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,22 @@
 export default function Home() {
-    return <> 
-        <h1>Oi Juliana!</h1>
+  return (
+    <>
+      <h1>Oi Juliana!</h1>
 
-        <h3> 
-            Eu estou aqui como forma de desafio do curso pra tentar te impactar
-            de alguma forma.
-        </h3>
+      <h3>
+        Eu estou aqui como forma de desafio do curso pra tentar te impactar de
+        alguma forma.
+      </h3>
 
-        <p>
-            Primeiramente, queria te agradecer por toda a motivação que você me
-            dá e todo o conforto que você traz. Sou totalmente apaixonado por você
-            e espero que possamos ter uma vida longa e bem sucedida juntos. Graças
-            a você eu tento todos os dias ser a minha melhor versão, mesmo que as 
-            vezes eu tenha que tomar um puxão de orelha seu, você ainda sempre traz
-            o melhor de mim. Te amo e muito obrigado por tudo que você tem feito
-            por mim! ❤️
-        </p>
+      <p>
+        Primeiramente, queria te agradecer por toda a motivação que você me dá e
+        todo o conforto que você traz. Sou totalmente apaixonado por você e
+        espero que possamos ter uma vida longa e bem sucedida juntos. Graças a
+        você eu tento todos os dias ser a minha melhor versão, mesmo que as
+        vezes eu tenha que tomar um puxão de orelha seu, você ainda sempre traz
+        o melhor de mim. Te amo e muito obrigado por tudo que você tem feito por
+        mim! ❤️
+      </p>
     </>
+  );
 }
